96d2be2a6e6db31a8aa45d897d4e66b19c91de54
dlicudi <dlicudi@gmail.com>
2026-04-11 01:01:27 +0200

    fix: update LightDimmer to use Sweep (renamed from UpDown)

--- cockpitdecks_ext/buttons/activation/dimmer.py
+++ cockpitdecks_ext/buttons/activation/dimmer.py
@@ -3,13 +3,13 @@
 import logging
 
 from cockpitdecks import DECK_ACTIONS
-from cockpitdecks.buttons.activation import UpDown
+from cockpitdecks.buttons.activation import Sweep
 
 logger = logging.getLogger(__name__)
 # logger.setLevel(logging.DEBUG)
 
 
-class LightDimmer(UpDown):
+class LightDimmer(Sweep):
     """Customized class to dim deck back lights according to up-down switch value"""
 
     ACTIVATION_NAME = "dimmer"
@@ -17,17 +17,17 @@ class LightDimmer(UpDown):
     REQUIRED_DECK_ACTIONS = [DECK_ACTIONS.PRESS, DECK_ACTIONS.LONGPRESS, DECK_ACTIONS.PUSH]
 
     _DIMMER_DEF = {"dimmer": {"type": "list", "schema": {"type": "integer"}, "meta": {"label": "Dataref"}}}
-    PARAMETERS = getattr(UpDown, "PARAMETERS", {}) | _DIMMER_DEF
-    SCHEMA = getattr(UpDown, "SCHEMA", {}) | _DIMMER_DEF
+    PARAMETERS = getattr(Sweep, "PARAMETERS", {}) | _DIMMER_DEF
+    SCHEMA = getattr(Sweep, "SCHEMA", {}) | _DIMMER_DEF
 
     def __init__(self, button: "Button"):
-        UpDown.__init__(self, button=button)
+        Sweep.__init__(self, button=button)
         self.dimmer = self._config.get("dimmer", [10, 90])
         self.deck_alt = self._config.get("deck")
         self.adjust_cockpit = self._config.get("adjust-cockpit", True)
 
     def activate(self, event):
-        currval = self.stop_current_value
+        currval = self.stop_current
         if currval is not None and 0 <= currval < len(self.dimmer):
             deck = self.button.deck
             if self.deck_alt is not None:
